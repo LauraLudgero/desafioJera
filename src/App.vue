968bc67f125240aef9502ba7b011ff7f0d0039fe
Laura Ludgero <lauraludgero@hotmail.com>
2022-05-20 22:59:33 -0400

    Integração com o Farebase

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
   </div>
 </template>
+
+
 <script>
 export default {
   created(){
     this.$http.get('https://api.themoviedb.org/3/discover/movie?api_key=d918157139c7bf7a666711e1c1826e36&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate')
-      .then(res => console.log(res.data))
+      .then(res => res.data.results)
+      .then(filme => this.$http.post("https://connectview-82647-default-rtdb.firebaseio.com/filmes.json", filme))
 
-  
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
